d74896548c8b4e09bd426bb7062375a9608717a5
danx <rushn24@gmail.com>
2023-07-15 18:29:23 -0400

    Step Three: The Question Page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
@@ -16,3 +16,16 @@ def show_survey():
     instructions = satisfaction_survey.instructions
 
     return render_template('home.html', title=title, instructions=instructions)
+
+@app.route('/start', methods=["POST"])
+def begin_survey():
+
+    return redirect('/questions/0')
+
+@app.route('/questions/<int:qid>')
+def show_question(qid):
+
+    question = satisfaction_survey.questions[qid]
+    choices =   question.choices
+
+    return render_template('question.html', question=question, question_num=qid, choices=choices)
