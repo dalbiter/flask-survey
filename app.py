30291ae11f74c65aae98f1083a931da2aa70830d
danx <rushn24@gmail.com>
2023-07-15 17:31:17 -0400

    Step Two: The Start Page

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_debugtoolbar import DebugToolbarExtension
+from surveys import satisfaction_survey
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret-key"
 debug = DebugToolbarExtension
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+RESPONSES = []
+
+@app.route('/')
+def show_survey():
+
+    title = satisfaction_survey.title
+    instructions = satisfaction_survey.instructions
+
+    return render_template('home.html', title=title, instructions=instructions)
